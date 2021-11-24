28bf85c1978f18a3e737ec2724110c4e3ab402dd
mudable <mudableshilpa@gmail.com>
2021-11-24 12:56:02 +0530

    Added : UC2 for multiple rides

--- src/test/java/TestCabInvoiceGenerator.java
+++ src/test/java/TestCabInvoiceGenerator.java
@@ -8,11 +8,19 @@ import junit.framework.Assert;
 
 public class TestCabInvoiceGenerator {
 	@Test
-	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
-		double distance = 2.0;
-		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time);
-		Assert.assertEquals(25, fare, 0.0);
-	}
-}
+	    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+	        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+	        double distance = 2.0;
+	        int time = 5;
+	        double fare = invoiceGenerator.calculateFare(distance, time);
+	        Assert.assertEquals(25, fare, 0.0);
+	    }
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+		 CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+	        double distance = 0.1;
+	        int time = 1;
+	        double fare = invoiceGenerator.calculateFare(distance, time);
+	        Assert.assertEquals(5, fare, 0.0);
+	    }
+		}
