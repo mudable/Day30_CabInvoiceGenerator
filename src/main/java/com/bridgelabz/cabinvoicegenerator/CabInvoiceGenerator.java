1e30e7d63d69f6cdcb8feaf7cad2cc89e671eaf5
mudable <mudableshilpa@gmail.com>
2021-11-27 12:17:43 +0530

    Added UC3 to invoice summery

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,15 +1,32 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
-	private static final double MINIMUM_COST_PER_KM = 10.0;
-	private static final int COST_PER_TIME = 1;
-	private static final int MINIMUM_FARE = 5;
+    //constant variables
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final int MINIMUM_FARE = 5;
 
-	public double calculateFare(double distance, int time) {
-		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-		if (totalFare < MINIMUM_FARE)
-			return MINIMUM_FARE;
-		else
-			return totalFare;
-	}
+    //calculate the total fare
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    //calculating Fare for Multiple rides
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
+
+    //calculating totalFare, average Fare for multiple rides
+    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
 }
---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+	public int time;
+	public double distance;
+
+	// parameterized Constructor
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
